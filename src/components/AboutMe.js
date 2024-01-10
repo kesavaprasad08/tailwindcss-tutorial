ba6c10ca141a27b9780301164852453686682ed5
kesavaprasad08 <kesav2661998@gmail.com>
2024-01-10 22:55:20 +0530

    few

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,8 +1,8 @@
 const AboutMe = () => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center bg-gray-950">
       <div className="flex flex-col items-center justify-center h-screen w-6/12 text-center ">
-        <p className="text-sky-500 text-3xl uppercase pb-5">A LITTLE BIT ABOUT ME</p>
+        <p className=" text-3xl uppercase pb-5">A LITTLE BIT ABOUT ME</p>
         <p className="pb-5">
           Front-end Web Developer adept at building responsive web applications.
           Passionate about Learning new technologies,bringing ideas to life and
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,27 +1,25 @@
-const Skills = ()=>{
-    return <div className="flex items-center justify-center">
-    <div className="flex flex-col items-center justify-center h-screen w-6/12 text-center ">
-      <p className="text-sky-500 text-3xl uppercase pb-5">SOME TECHNOLOGIES I'VE WORKED WITH:</p>
-      <p className="pb-5">
-        <div className="grid sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-4">
-            <div>
-                HTML
-            </div><div>
-                CSS
-            </div><div>
-                JAVASCRIPT
-            </div><div>
-                REACT JS
-            </div><div>
-                REDUX
-            </div><div>
-                TAILWIND
-            </div>
-            
-        </div>
-      </p>
+const Skills = () => {
+  return (
+    <div className="flex items-center justify-center bg-black">
+      <div className="flex flex-col items-center justify-center h-screen w-6/12 text-center ">
+        <p className=" text-3xl uppercase pb-5">
+          SOME TECHNOLOGIES I'VE WORKED WITH:
+        </p>
+        <p className="pb-5">
+          <div className="grid sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-4">
+            <div className="border-white border p-5 rounded-full">HTML</div>
+            <div className="border-white border p-5 rounded-full">CSS</div>
+            <div className="border-white border  p-5 rounded-full">JAVASCRIPT</div>
+            <div className="border-white border  p-5 rounded-full">REACT JS</div>
+            <div className="border-white border  p-5 rounded-full">REDUX</div>
+            <div className="border-white border  p-5 rounded-full">TAILWIND</div>
+            <div className="border-white border  p-5 rounded-full">GIT + GITHUB</div>
+            <div className="border-white border  p-5 rounded-full">BOOTSTRAP</div>
+          </div>
+        </p>
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
 export default Skills;
