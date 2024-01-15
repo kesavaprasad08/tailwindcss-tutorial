6201322a4bc451ad07357ee2d65cbba96497a0cb
kesavaprasad08 <kesav2661998@gmail.com>
2024-01-15 17:51:27 +0530

    da

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,40 @@
-const Contact =() =>{
-    return <div className="flex items-center justify-center">
-    <div className="flex flex-col items-center justify-center h-screen w-6/12 text-center ">
-      <p className="text-sky-500 text-3xl uppercase pb-5">GET IN TOUCH!</p>
-      <p className="pb-5">
-       
-      </p>
+const Contact = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center h-screen w-6/12 text-center ">
+        <p className="text-sky-500 text-3xl uppercase pb-5">GET IN TOUCH!</p>
+        <form className="w-96">
+          <input
+            type="text"
+            id="first_name"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Name"
+            required
+          />
+          <input
+            type="text"
+            id="first_name"
+            class="bg-gray-50 mt-5 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="E-Mail"
+            required
+          />
+          <input
+            type="text"
+            id="first_name"
+            class="bg-gray-50 mt-5 border h-16  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Message"
+            required
+          />
+          <button
+            type="button"
+            class="text-white mt-5 bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
 export default Contact;
